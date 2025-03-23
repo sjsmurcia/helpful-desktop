59c43933359a45e40c301abbbde2769eb2e1464f
sjsmurcia <128265045+sjsmurcia@users.noreply.github.com>
2025-03-22 23:32:38 -0600

    viewscontroller

--- app/controllers/viewsController.php
+++ app/controllers/viewsController.php
@@ -1,8 +1,18 @@
 <?php
-    namespace app\controllers;
-    use app\models\viewsModel;
 
-    class viewsController extends viewsModel {
-        
+namespace app\controllers;
+
+use app\models\viewsModel;
+
+class viewsController extends viewsModel
+{
+    public function getviewController($view)
+    {
+        if ($view != "") {
+            $respuesta = $this->getViewsModels($view);
+        } else {
+            $respuesta = "login";
+        }
+        return $respuesta;
     }
-?>
+}
